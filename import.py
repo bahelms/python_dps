06372985685ffeb747184d4731cfc18cc6f1fee6
bahelms <jimbonkgit@gmail.com>
2016-08-25 21:25:02 -0400

    adds csv reader

--- import.py
+++ import.py
@@ -1,1 +1,9 @@
-print("hey there")
+import csv
+
+def sanitize(record):
+    return record
+
+with open("tmp/HA_data.csv", newline="") as csv_file:
+    for row in csv.DictReader(csv_file):
+        print(sanitize(row))
+
