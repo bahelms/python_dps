1ee426ca701064d015f81f076a466b620f2703cd
bahelms <jimbonkgit@gmail.com>
2016-09-09 06:22:32 -0400

    moves database config into yaml file and adds dev environment

--- README.md
+++ README.md
@@ -10,6 +10,5 @@ Monitors a directory for CSV files and processes anything it finds.
 * Have tests use their own test database
 * Unique constraint violation: Everything is an insert; updates aren't automatic
 * Convert delete code into deleted_at timestamp
-* Move table creation into Alembic migrations
-* Move database config in setup.py into config file
 * Dynamically add model columns from config
+* Move table creation into Alembic migrations?
--- setup.py
+++ setup.py
@@ -1,16 +1,25 @@
+import yaml
+import os
 from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
 from dps.models import Base, spec_attribute, specatt
 
-db_conf = "postgres://postgres:postgres@postgres/dps"
-engine = create_engine(db_conf, echo=True)
+env = os.environ.get("DPS_ENV", default="development")
+
+with open("config/database.yml") as f:
+    config = yaml.load(f.read())[env]
+
+db_conn_template = "postgres://{username}:{password}@{host}:{port}/{database}"
+db_conn = db_conn_template.format(**config)
+engine = create_engine(db_conn, echo=True)
 
 # Check if database exists
 try:
     conn = engine.connect()
 except Exception as e:
     # Create database if it doesn't exist
-    db_conf = "postgres://postgres:postgres@postgres/postgres"
-    postgres_engine = create_engine(db_conf, echo=True)
+    config["database"] = "postgres"
+    db_conn = db_conn_template.format(**config)
+    postgres_engine = create_engine(db_conn, echo=True)
     pg_conn = postgres_engine.connect()
     pg_conn.execute("commit")
     pg_conn.execute("create database dps")
